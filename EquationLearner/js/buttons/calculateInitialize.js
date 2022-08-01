8fc22fcf8d6483e6521f11f8213488d01a9ab88f
AmitBitansky1 <106779292+AmitBitansky1@users.noreply.github.com>
2022-08-01 02:00:40 -0400

    simplify calculateInitialize

--- EquationLearner/js/buttons/calculateInitialize.js
+++ EquationLearner/js/buttons/calculateInitialize.js
@@ -1,31 +1,40 @@
 import calculate from "../calculate.js";
 
+function setOperation(operationText) {
+  if (operationText == "+") return "add";
+  if (operationText == "-") return "subtract";
+  if (operationText == "x") return "multiply";
+  if (operationText == "/") return "divide";
+}
+
+function variableIdentityCheck(variableText) {
+  if (variableText == "x") {
+    variableText = "1x";
+  }
+  if (variableText == "-x") {
+    variableText = "-1x";
+  }
+  return variableText;
+}
+
+function variableTextToFloat(variableText, type) {
+  if (type == "variable") return parseFloat(variableText.split("x")[0]);
+  if (type == "constant") return parseFloat(variableText);
+}
+
 function calculateInitialize() {
   let operationText = document.getElementById("operation").textContent;
   let variableText = document.getElementById("variable").textContent;
 
   let type = variableText.includes("x") ? "variable" : "constant";
+  let operation = setOperation(operationText);
 
-  let operation;
-  if (operationText == "+") operation = "add";
-  if (operationText == "-") operation = "subtract";
-  if (operationText == "x") operation = "multiply";
-  if (operationText == "/") operation = "divide";
+  variableText = variableIdentityCheck(variableText);
+  let variableFloat = variableTextToFloat(variableText, type);
 
-  let newVariableText;
-  if (type == "variable")
-    if (variableText == "x") {
-      variableText = "1x";
-    }
-  if (variableText == "-x") {
-    variableText = "-1x";
-  }
-  newVariableText = parseFloat(variableText.split("x")[0]);
-  if (type == "constant") newVariableText = parseFloat(variableText);
-
-  if (newVariableText < 0) {
+  if (variableFloat < 0) {
     if (operation == "subtract") {
-      newVariableText *= -1;
+      variableFloat *= -1;
       operation = "add";
     }
   }
