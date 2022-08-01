a60813996c42b1b256e1d2485c44e5be1d24eaca
AmitBitansky1 <106779292+AmitBitansky1@users.noreply.github.com>
2022-08-01 02:12:42 -0400

    buttonFunctionality Update

--- EquationLearner/js/buttons/buttonFunctionality.js
+++ EquationLearner/js/buttons/buttonFunctionality.js
@@ -2,21 +2,32 @@ import calculateInitialize from "./calculateInitialize.js";
 
 function buttonFunctionality() {
   const variableButtons = document.getElementsByClassName("variable");
+  addVariableButtonFunctionality(variableButtons);
+
+  const operationButtons = document.getElementsByClassName("operation");
+  addOperationButtonFunctionality(operationButtons);
+
+  document
+    .getElementById("calculate")
+    .addEventListener("click", calculateInitialize);
+}
+
+function addVariableButtonFunctionality(variableButtons) {
   for (let i = 0; i < variableButtons.length; i++) {
     variableButtons[i].addEventListener("click", function () {
       document.getElementById("variable").textContent =
         variableButtons[i].textContent;
     });
   }
-  const operationButtons = document.getElementsByClassName("operation");
+}
+
+function addOperationButtonFunctionality(operationButtons) {
   for (let i = 0; i < operationButtons.length; i++) {
     operationButtons[i].addEventListener("click", function () {
       document.getElementById("operation").textContent =
         operationButtons[i].value;
     });
   }
-  document
-    .getElementById("calculate")
-    .addEventListener("click", calculateInitialize);
 }
+
 export default buttonFunctionality;
