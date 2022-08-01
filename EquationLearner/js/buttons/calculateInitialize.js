f7aa13ea77fd0a3eea8e6a83705a5b50f795c072
AmitBitansky1 <106779292+AmitBitansky1@users.noreply.github.com>
2022-08-01 02:07:37 -0400

    flipCalculate Initialize

--- EquationLearner/js/buttons/calculateInitialize.js
+++ EquationLearner/js/buttons/calculateInitialize.js
@@ -1,5 +1,25 @@
 import calculate from "../calculate.js";
 
+function calculateInitialize() {
+  let operationText = document.getElementById("operation").textContent;
+  let variableText = document.getElementById("variable").textContent;
+
+  let type = variableText.includes("x") ? "variable" : "constant";
+  let operation = setOperation(operationText);
+
+  variableText = variableIdentityCheck(variableText);
+  let variableFloat = variableTextToFloat(variableText, type);
+
+  if (variableFloat < 0) {
+    if (operation == "subtract") {
+      variableFloat *= -1;
+      operation = "add";
+    }
+  }
+
+  calculate(operation, variableFloat, type);
+}
+
 function setOperation(operationText) {
   if (operationText == "+") return "add";
   if (operationText == "-") return "subtract";
@@ -20,26 +40,6 @@ function variableIdentityCheck(variableText) {
 function variableTextToFloat(variableText, type) {
   if (type == "variable") return parseFloat(variableText.split("x")[0]);
   if (type == "constant") return parseFloat(variableText);
-}
-
-function calculateInitialize() {
-  let operationText = document.getElementById("operation").textContent;
-  let variableText = document.getElementById("variable").textContent;
-
-  let type = variableText.includes("x") ? "variable" : "constant";
-  let operation = setOperation(operationText);
-
-  variableText = variableIdentityCheck(variableText);
-  let variableFloat = variableTextToFloat(variableText, type);
-
-  if (variableFloat < 0) {
-    if (operation == "subtract") {
-      variableFloat *= -1;
-      operation = "add";
-    }
-  }
-
-  calculate(operation, variableFloat, type);
 }
 
 export default calculateInitialize;
