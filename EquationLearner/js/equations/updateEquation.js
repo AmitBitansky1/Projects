a423c18e84d8cd06edee982345bc5c09f8af1049
AmitBitansky1 <106779292+AmitBitansky1@users.noreply.github.com>
2022-08-01 04:11:02 -0400

    clean updateEquation

--- EquationLearner/js/equations/updateEquation.js
+++ EquationLearner/js/equations/updateEquation.js
@@ -6,59 +6,93 @@ let equation = [0, 0, 0, 0];
 
 function updateEquation(newEquation, method = "") {
   if (method == "set") {
-    equation[0] = newEquation[0];
-    equation[1] = newEquation[1];
-    equation[2] = newEquation[2];
-    equation[3] = newEquation[3];
+    setEquation(newEquation);
   }
   if (method == "get") {
     return equation;
   }
   if (method.includes("operation")) {
     if (method.includes("add")) {
-      equation[0] += newEquation[0];
-      equation[1] += newEquation[1];
-      equation[2] += newEquation[2];
-      equation[3] += newEquation[3];
+      addEquation(newEquation);
     }
     if (method.includes("subtract")) {
-      equation[0] -= newEquation[0];
-      equation[1] -= newEquation[1];
-      equation[2] -= newEquation[2];
-      equation[3] -= newEquation[3];
+      subtractEquation(newEquation);
     }
     if (method.includes("multiply")) {
-      equation[0] *= newEquation[0];
-      equation[1] *= newEquation[1];
-      equation[2] *= newEquation[2];
-      equation[3] *= newEquation[3];
+      multiplyEquation(newEquation);
     }
     if (method.includes("divide")) {
-      if (equation[0] != 0) equation[0] /= newEquation[0];
-      if (equation[1] != 0) equation[1] /= newEquation[1];
-      if (equation[2] != 0) equation[2] /= newEquation[2];
-      if (equation[3] != 0) equation[3] /= newEquation[3];
+      divideEquation(newEquation);
     }
-    createRegularEquation(
-      Math.floor(equation[0] * 100) / 100,
-      Math.floor(equation[1] * 100) / 100,
-      Math.floor(equation[2] * 100) / 100,
-      Math.floor(equation[3] * 100) / 100,
-      "regular"
-    );
-    createVariableButtons(
-      Math.floor(equation[0] * 100) / 100,
-      Math.floor(equation[1] * 100) / 100,
-      Math.floor(equation[2] * 100) / 100,
-      Math.floor(equation[3] * 100) / 100
-    );
+    createEquation();
+    createAllVariableButtons();
   }
+  if (isXIsolated()) {
+    document.getElementById("solved").textContent = "YOU SOLVED THE EQUATION!";
+  }
+}
+
+function setEquation(newEquation) {
+  equation[0] = newEquation[0];
+  equation[1] = newEquation[1];
+  equation[2] = newEquation[2];
+  equation[3] = newEquation[3];
+}
+
+function addEquation(newEquation) {
+  equation[0] += newEquation[0];
+  equation[1] += newEquation[1];
+  equation[2] += newEquation[2];
+  equation[3] += newEquation[3];
+}
+
+function subtractEquation(newEquation) {
+  equation[0] -= newEquation[0];
+  equation[1] -= newEquation[1];
+  equation[2] -= newEquation[2];
+  equation[3] -= newEquation[3];
+}
+
+function multiplyEquation(newEquation) {
+  equation[0] *= newEquation[0];
+  equation[1] *= newEquation[1];
+  equation[2] *= newEquation[2];
+  equation[3] *= newEquation[3];
+}
+
+function divideEquation(newEquation) {
+  if (equation[0] != 0) equation[0] /= newEquation[0];
+  if (equation[1] != 0) equation[1] /= newEquation[1];
+  if (equation[2] != 0) equation[2] /= newEquation[2];
+  if (equation[3] != 0) equation[3] /= newEquation[3];
+}
+
+function createEquation() {
+  createRegularEquation(
+    Math.floor(equation[0] * 100) / 100,
+    Math.floor(equation[1] * 100) / 100,
+    Math.floor(equation[2] * 100) / 100,
+    Math.floor(equation[3] * 100) / 100,
+    "regular"
+  );
+}
+
+function createAllVariableButtons() {
+  createVariableButtons(
+    Math.floor(equation[0] * 100) / 100,
+    Math.floor(equation[1] * 100) / 100,
+    Math.floor(equation[2] * 100) / 100,
+    Math.floor(equation[3] * 100) / 100
+  );
+}
+
+function isXIsolated() {
   if (
     (equation[0] == 1 && equation[1] == 0 && equation[2] == 0) ||
     (equation[0] == 0 && equation[2] == 1 && equation[3] == 0)
-  ) {
-    document.getElementById("solved").textContent = "YOU SOLVED THE EQUATION!";
-  }
+  )
+    return true;
+  return false;
 }
 
 export default updateEquation;
